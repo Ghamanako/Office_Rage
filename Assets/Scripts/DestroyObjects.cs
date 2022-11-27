d66a09754656dfaf84b1fe1e145b7e29b1b4f823
Ghamanako <73056948+Ghamanako@users.noreply.github.com>
2022-11-27 15:18:58 +0700

    masukin model 3 ma tambahan untuk lose condition

--- Assets/Scripts/DestroyObjects.cs
+++ Assets/Scripts/DestroyObjects.cs
@@ -7,19 +7,28 @@ public class DestroyObjects : MonoBehaviour
     public GameObject Prefab;
     int countDestroyedObjectValue = 1;
     GameManager game;
-
+    Ragebar ragebar;
+    BoxCollider colliders;
     private void Start()
     {
         game = GameObject.Find("GameEventSystem").GetComponent<GameManager>();
+        ragebar = GameObject.Find("Player").GetComponent<Ragebar>();
+        colliders = GetComponent<BoxCollider>();
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")&& ragebar.RageModeOn)
         {
+            colliders.isTrigger = true;
             Destroy(gameObject);
             Instantiate(Prefab, transform.position, transform.rotation);
             game.updatevalue(countDestroyedObjectValue);
             Debug.Log(countDestroyedObjectValue);
+        }
+        else
+        {
+            if(other.CompareTag("Player"))
+            colliders.isTrigger = false;
         }
     }
 }
